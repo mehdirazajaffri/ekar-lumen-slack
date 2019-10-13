17814d60246386fac9164e4526596907044e5eb4
Mehdi Raza <mehdi.raza@venturedive.com>
2019-10-13 20:35:38 +0500

    ekar controller

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Helpers;
+
+class Controller extends BaseController
+{
+    public function ekar(Request $request)
+    {
+        // putting validation for the safe side
+        $this->validate($request, [
+            'event' => 'required',
+            'event.type' => 'required',
+            'event.channel' => 'required'
+        ]);
+        // Normalizing payLoad
+        $payload = $request->get('event');
+
+        if ($payload['type'] == "app_mention") {
+            $text = $payload['text'];
+
+            if (strpos($text, 'solve for') !== false) {
+                // Remove string before the number.
+                $text = substr($text,strpos($text, 'solve for'));
+                // get integer value of number from the string
+                $num = Helpers::getNumberFromString($text);
+                if ($num > 0 && $num < 1000) {
+                    $sum = Helpers::euler($num);
+                    // Time to Send Response
+                    Helpers::sendCurlToSlack([
+                        "text" => "Sir! Your sum is " . $sum . " Thank you!",
+                        "channel" => $payload['channel']
+                    ]);
+                } else {
+                    Helpers::sendCurlToSlack([
+                        "text" => "Sir! Your number should be greater than 0 and less than 1000, Thank you!",
+                        "channel" => $payload['channel']
+                    ]);
+                }
+            }
+        }
+
+        exit;
+    }
+}
---
+++ app/Libraries/Helper.php
@@ -0,0 +1,46 @@
+<?php
+class Helpers
+{
+    public static function euler($number)
+    {
+        $numbers = array_filter(range(1, $number - 1), function ($n) {
+            return ($n % 3 == 0) || ($n % 5 == 0);
+        });
+        $res = array_sum($numbers);
+
+        return $res;
+    }
+
+    public static function sendCurlToSlack($response)
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://slack.com/api/chat.postMessage",
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => json_encode($response),
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer xoxb-793684583653-780912309122-kVuw7qkQx6jGtfjw5TwTU14r",
+                "Content-Type: application/json",
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo "";
+        }
+    }
+
+    public static function getNumberFromString($string){
+        $int = intval(preg_replace('/[^0-9]+/', '', $string), 10);
+        return $int;
+    }
+}
