a36575361d8f8b60ffae49fdd3618caf33979a39
Mehdi Raza <mehdi.raza@venturedive.com>
2019-10-14 10:49:19 +0500

    verification on slavk api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,44 +10,6 @@ class Controller extends BaseController
 {
     public function ekar(Request $request)
     {
-        // putting validation for the safe side
-        $this->validate($request, [
-            'event' => 'required',
-            'event.type' => 'required',
-            'event.channel' => 'required'
-        ]);
-
-        error_log(json_encode($request->all()));
-
-        // Normalizing payLoad
-        $payload = $request->get('event');
-
-        if ($payload['type'] == "app_mention") {
-            $text = $payload['text'];
-            $keyword = "solve";
-
-            if (strpos($text, $keyword) !== false) {
-                // Remove string before the number.
-                $text = substr($text, strpos($text, $keyword));
-                // get integer value of number from the string
-                $num = Helpers::getNumberFromString($text);
-
-                if ($num > 0 && $num < 1000) {
-                    $sum = Helpers::euler($num);
-                    // Time to Send Response
-                    Helpers::sendCurlToSlack([
-                        "text" => "Sir! Your sum is " . $sum . " Thank you!",
-                        "channel" => $payload['channel']
-                    ]);
-                } else {
-                    Helpers::sendCurlToSlack([
-                        "text" => "Sir! Your number should be greater than 0 and less than 1000, Thank you!",
-                        "channel" => $payload['channel']
-                    ]);
-                }
-            }
-        }
-
-        exit;
+        return "Good";
     }
 }
